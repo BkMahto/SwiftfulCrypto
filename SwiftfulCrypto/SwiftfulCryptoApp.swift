1a9642f8cf76779c5a4fc208f7b2451ecc1e10d6
Bandan Kumar Mahto <159515893+BkMahto@users.noreply.github.com>
2024-05-21 12:34:46 +0530

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by elluminati on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
